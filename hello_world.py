d83e6aef801b2836f2e919344b35cf610ad8f304
gauthamdasu <gauthamdasu@gmail.com>
2019-02-20 21:20:08 +0530

    first hello world program

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#Hi this is a sample hello world program which takes name as input and prints the name as output in python
+# here '#' stands for single comment .
+
+name = input("Hello what is your name?\n") # take input from the user through the string 'name'
+
+'''
+you can use ''' ''' for commenting multiple lines.
+'''
+
+print("Hello "+ name + " ")
+
+
+
+
