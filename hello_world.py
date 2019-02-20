5890f6a74bf0006c1b52fcc67247245b4df634bb
gauthamdasu <gauthamdasu@gmail.com>
2019-02-20 21:32:21 +0530

    modified hello world

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,14 @@
 #Hi this is a sample hello world program which takes name as input and prints the name as output in python
 # here '#' stands for single comment .
-
-name = input("Hello what is your name?\n") # take input from the user through the string 'name'
-
+import sys
 '''
 you can use ''' ''' for commenting multiple lines.
 '''
-
-print("Hello "+ name + " ")
-
-
-
-
+if sys.version_info[0] < 3:
+	name = raw_input("Hello what is your name?\n") # take input from the user through the string 'name'
+	print ("Hello "+ name + " ")
+	print ("You are using python version below 3\n")
+else :
+	name = input("Hello what is your name?\n") # take input from the user through the string 'name'
+	print("Hello "+ name + " ")
+	print ("You are using python version above 3\n")
